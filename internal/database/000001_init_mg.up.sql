206e4f2876652a139992da9bf52044c40e73f1f7
vsnaichuk <v.snaichuk@gmail.com>
2024-09-27 14:45:49 +0200

    chore: create init migration using golang-migrate
    https://www.freecodecamp.org/news/database-migration-golang-migrate/

---
+++ internal/database/000001_init_mg.up.sql
@@ -0,0 +1,145 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.8 (Homebrew)
+-- Dumped by pg_dump version 15.8 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
+-- Name: get_or_create_user(text, text, text, text); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.get_or_create_user(p_oauth_id text, p_name text, p_avatar_url text, p_email text) RETURNS TABLE(id uuid, name text, avatar_url text)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    -- Try to insert a new user, ignore the insert if the user already exists
+    INSERT INTO users (id, oauth_id, name, avatar_url, email)
+    VALUES (uuid_generate_v4(), p_oauth_id, p_name, p_avatar_url, p_email)
+    ON CONFLICT (email) DO NOTHING;
+
+    -- Retrieve the user
+    RETURN QUERY 
+    SELECT users.id, users.name, users.avatar_url
+    FROM users
+    WHERE users.email = p_email;
+END;
+$$;
+
+
+ALTER FUNCTION public.get_or_create_user(p_oauth_id text, p_name text, p_avatar_url text, p_email text) OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: events; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.events (
+    id uuid NOT NULL,
+    name text,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    owner uuid NOT NULL,
+    image_url text
+);
+
+
+ALTER TABLE public.events OWNER TO postgres;
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.schema_migrations (
+    version bigint NOT NULL,
+    dirty boolean NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    oauth_id text,
+    name text NOT NULL,
+    avatar_url text,
+    email text NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: users unique_users_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_users_email UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: events events_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_owner_fkey FOREIGN KEY (owner) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
